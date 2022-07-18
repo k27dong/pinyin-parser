b31fe43c9d372a0f0d510522f164a5be8e9f9704
k27dong <me@kefan.me>
2022-07-18 11:40:54 -0400

    bugfix

--- src/parse.js
+++ src/parse.js
@@ -1,9 +1,9 @@
 ;(function () {
   // CORE
   parse = function (message, options = 0) {
-    const TONE = require("./const"),
-      INITIAL = require("./const"),
-      FINAL = require("./const")
+    const TONE = require("./const").TONE,
+      INITIAL = require("./const").INITIAL,
+      FINAL = require("./const").FINAL
 
     const set_char = (str, index, char) => {
       return str.substring(0, index) + char + str.substring(index + 1)
